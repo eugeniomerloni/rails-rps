8c116d48bef883df5ced917270b964b50283e42e
eugeniomerloni <164801392+eugeniomerloni@users.noreply.github.com>
2024-04-21 00:00:35 +0000

    100% grade

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,48 @@
+class ZebraController < ApplicationController
+  def homepage
+    
+    render({ :template => "game_templates/homepage"})
+  end
+
+  def giraffe
+    @random_move = ["rock", "paper", "scissors"].sample
+
+    if @random_move == "rock"
+      @outcome = "tied"
+    elsif @random_move == "paper"
+      @outcome = "lost"
+    else 
+      @outcome = "won"
+    end
+
+    render({ :template => "game_templates/play_rock"})
+  end
+
+  def elephant
+    @random_move = ["rock", "paper", "scissors"].sample
+
+    if @random_move == "rock"
+      @outcome = "won"
+    elsif @random_move == "paper"
+      @outcome = "tied"
+    else 
+      @outcome = "lost"
+    end
+    
+    render({ :template => "game_templates/play_paper"})
+  end
+
+  def lion
+    @random_move = ["rock", "paper", "scissors"].sample
+
+    if @random_move == "rock"
+      @outcome = "lost"
+    elsif @random_move == "paper"
+      @outcome = "won"
+    else 
+      @outcome = "tied"
+    end
+    
+    render({ :template => "game_templates/play_scissors"})
+  end
+end
